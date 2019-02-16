b8c1f33f365b87eec7c5050706e98e4ac54d7b4a
KimKosit <user_kho_kim@hotmail.com>
2019-02-16 17:14:17 +0700

    Add home screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,14 +4,114 @@ class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return null;
+    return HomeState();
   }
 }
 
 class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        bottomNavigationBar: TabBar(
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.view_compact),
+            ),
+            Tab(
+              icon: Icon(Icons.notifications),
+            ),
+            Tab(
+              icon: Icon(Icons.explore),
+            ),
+            Tab(
+              icon: Icon(Icons.person),
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white30,
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          title: Text(
+            'Home',
+            style: TextStyle(fontSize: 12),
+          ),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Text(
+                  'Home',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              color: Colors.white,
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  'Notify',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              color: Colors.white,
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  'Map',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              color: Colors.white,
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  'Profile',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              color: Colors.white,
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  'Setup',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
